f0206acd771829bbfb158027465319472b484f08
Federic579 <53410827+Federic579@users.noreply.github.com>
2024-03-14 11:45:21 +0000

    addTail & addHead

---
+++ L_Lista/Lista.java
@@ -0,0 +1,44 @@
+package L_Lista;
+
+public class Lista {
+
+    Nodo radice;
+    
+    public Lista() {
+        radice = null;
+    }
+
+    public boolean isEmpty() {
+        return radice==null;
+    }
+
+    public void addTail( Nodo n ) {
+        if (isEmpty()) {
+            radice = n;
+        } else {
+            Nodo p = radice;
+            while (p.getSuccessivo()!=null) p = p.getSuccessivo();
+            p.setSuccessivo( n );
+        }
+    }
+
+    public void addHead( Nodo n){
+        if(isEmpty()){
+            radice = n;
+        } else{
+            n.setSuccessivo(radice);
+            radice = n;
+        }
+    }
+
+    public String toString() {
+        String s = "Elementi della lista: ";
+        Nodo p = radice;
+        while (p!=null) {
+            s += p + " ";
+            p = p.getSuccessivo();
+        }
+        s += "End!\n";
+        return s;
+    }
+}
