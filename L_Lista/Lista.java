f5c426aa1a4b937f9391d7739267e3541c2dc759
Federic579 <53410827+Federic579@users.noreply.github.com>
2024-03-15 09:49:10 +0000

    addafter

--- L_Lista/Lista.java
+++ L_Lista/Lista.java
@@ -1,6 +1,36 @@
 package L_Lista;
 
+import java.util.Iterator;
+
 public class Lista {
+
+    class Iteratore{
+        private Nodo nodo;
+
+        private Iteratore(Nodo nodo){
+            this.nodo = nodo;
+        }
+
+        public boolean hasNext(){
+            return nodo != null;
+        }
+
+        public Nodo next(){
+            if(nodo == null){
+                return null;
+            }
+
+            Nodo result = new Nodo(nodo.getValore(), nodo.getSuccessivo());
+            return result;
+        }
+    }
+
+
+    public Iteratore getIterator(){
+        Iteratore i = new Iteratore(radice);
+        return i;
+    }
+
 
     Nodo radice;
     
@@ -8,9 +38,11 @@ public class Lista {
         radice = null;
     }
 
+
     public boolean isEmpty() {
         return radice==null;
     }
+
 
     public void addTail( Nodo n ) {
         if (isEmpty()) {
@@ -22,6 +54,7 @@ public class Lista {
         }
     }
 
+
     public void addHead( Nodo n){
         if(isEmpty()){
             radice = n;
@@ -30,6 +63,7 @@ public class Lista {
             radice = n;
         }
     }
+
 
     public void addSorted( Nodo n){
         if ( isEmpty()){
@@ -52,10 +86,78 @@ public class Lista {
             p1 = p2;
             p2 = p1.getSuccessivo();
         }
-
         n.setSuccessivo(p2);
         p1.setSuccessivo(n);
+    }
 
+
+    boolean addAfter(Nodo n, int pos){
+        if (isEmpty()) {
+        return false;
+        } else{
+            Nodo iniziale = radice;
+            int count = 0;
+
+            while(iniziale != null){
+                if(count == pos){
+                    Nodo nextNode = iniziale.getSuccessivo();
+                    iniziale.setSuccessivo(n);
+                    n.setSuccessivo(nextNode);
+                    return true;
+                }
+
+                iniziale = iniziale.getSuccessivo();
+                count++;
+            }
+
+            return false;
+        }
+
+        /*addAfter del prof:
+        public boolean addAfter(int pos, Nodo n){
+            Iteratore iter = this.getIterator();
+            int i;
+            Nodo npos;
+            for(i = 0; i < pos; i++){
+                if (iter.hasNext()) {
+                    npos = iter.next();
+                } else{
+                    return false;
+                }
+            }
+            n.setSuccessivo(npos.getSuccessivo());
+            npos.setSuccessivo(n);
+            return true;
+
+            
+        }
+        */
+    }
+
+    boolean remove_Pos(int pos){
+
+    }
+
+    boolean removeValue(int value){
+        int index = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (i == value) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1) {
+            return false; // il valore da rimuovere non è presente nell'array
+        }
+        
+        for (int i = index; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        
+        array[array.length - 1] = 0; // rimuovi l'ultimo elemento
+
+        return true;
     }
     
 
--- L_Lista/Main.java
+++ L_Lista/Main.java
@@ -15,15 +15,20 @@ public class Main {
         l.addHead(new Nodo(2024, null));
 
         System.out.println( l );
-    } 
+    
 
-    l.addSorted(new Nodo(300, null));
-    l.addSorted(new Nodo(250, null));
-    l.addSorted(new Nodo(350, null));
-    l.addSorted(new Nodo(320, null));
-    l.addSorted(new Nodo(100, null));
-    l.addSorted(new Nodo(290, null));
-    l.addSorted(new Nodo(450, null));
+        l.addSorted(new Nodo(300, null));
+        l.addSorted(new Nodo(250, null));
+        l.addSorted(new Nodo(350, null));
+        l.addSorted(new Nodo(320, null));
+        l.addSorted(new Nodo(100, null));
+        l.addSorted(new Nodo(290, null));
+        l.addSorted(new Nodo(450, null));
 
+        System.out.println(l);
+
+        l.addAfter(3, new Nodo(1492, null));
+        System.out.println(l);
+    }
 
 }
